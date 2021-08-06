a74ff2b27ec9fe256fe911f4329f89c9cfca5ad2
Wayu Bangkamed <gene20898@hotmail.com>
2021-08-06 18:18:43 +0700

    Add cart page

--- src/app/components/cart/cart.component.ts
+++ src/app/components/cart/cart.component.ts
@@ -1,15 +1,37 @@
 import { Component, OnInit } from '@angular/core';
-
+import { CartService } from 'src/app/services/cart.service';
+import { Product } from 'src/app/models/Product';
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-cart',
   templateUrl: './cart.component.html',
   styleUrls: ['./cart.component.css']
 })
 export class CartComponent implements OnInit {
+  cart: Product[] = [];
+  total: number = 0;
 
-  constructor() { }
+  constructor(
+    private cartService: CartService,
+    private router: Router
+    ) { }
 
   ngOnInit(): void {
+    this.cart = this.cartService.getCartItems();
+    this.totalPrice();
   }
 
+  amountChange(item: Product):void {
+    this.cartService.updateItem(item);
+    this.cart = this.cartService.getCartItems();
+    this.totalPrice();
+  }
+  
+  totalPrice():void {
+    this.total = this.cart.reduce((total,item) => {return total + item.amount*item.price;}, 0);
+    this.total = parseFloat(this.total.toFixed(2));
+  }
+  onSubmit(): void {
+    this.router.navigate(['/confirm']);
+  }
 }
--- src/app/services/cart.service.ts
+++ src/app/services/cart.service.ts
@@ -1,9 +1,33 @@
 import { Injectable } from '@angular/core';
-
+import { Product } from '../models/Product';
 @Injectable({
   providedIn: 'root'
 })
 export class CartService {
-
+  cart:Product[] = [];
   constructor() { }
+
+  updateItem(product: Product): void {
+    const amount = product.amount;
+    const id = product.id;
+    const findingItem = this.cart.find(item => {return item.id === id}) as Product;
+    if(findingItem == undefined){
+      this.cart.push(product);
+    }
+    else{
+      if(amount == 0){
+        this.cart = this.cart.filter(item => {return item.id != id})
+        console.log("amount = 0");
+      }
+      else{
+        findingItem.amount = amount;
+        console.log("amount enter" + amount + " and: " + findingItem.amount);
+      }
+    }
+    console.log(this.cart);
+  }
+
+  getCartItems(): Product[] {
+    return this.cart;
+  }
 }
